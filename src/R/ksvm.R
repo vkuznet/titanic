e2f81b032b31436366f6aded75d2cfa3ff725d74
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-19 11:54:55 -0400

    Use drop function; use explicit name for prediction

--- src/R/ksvm.R
+++ src/R/ksvm.R
@@ -11,9 +11,9 @@ file.name <- "model.csv"
 df <- read.csv(paste0(my.path, file.name), header=T)
 
 # exclude id columne to work with ML
-train.df <- df[2:ncol(df)]
-# during training we use the same dataset, but exclude classification var (last one)
-test.df <- train.df[2:ncol(train.df)-1]
+train.df <- drop(df, c("id", "PassengerId"))
+# during training we use the same dataset, but exclude classification var
+test.df <- drop(df, c("id", "PassengerId", "Survived"))
 
 # this is an example on how to split data into train/test datasets
 #index <- 1:nrow(df)
@@ -37,11 +37,12 @@ cross <- 10
 cost <- 1
 
 # run svm algorithm
-model <- ksvm(Survived~., data=train.df, type=type, cross=cross, kernel=k, C=cost)
-print(model)
+ksvm.model <- ksvm(Survived~., data=train.df,
+            type=type, cross=cross, kernel=k, C=cost, prob.model=T)
+print(ksvm.model)
 
 # the last column of this dataset is what we'll predict, so we'll exclude it
-ksvm.pred <- predict(model, test.df)
+ksvm.pred <- predict(ksvm.model, test.df)
 
 # print confugtion matrix
 conf.matrix(train.df$Survived, ksvm.pred)
