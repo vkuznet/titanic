2c58fe8b40b659d93d6bc5f51ac4f2ad3977e6b2
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-20 11:35:57 -0400

    Add new helper functions to cut original dataset into two pieces; run.rf, run.ksvm, run.svm and cut.proc

--- src/R/helper.R
+++ src/R/helper.R
@@ -162,10 +162,130 @@ roc <- function(fit, df, fname) {
 # helper function to write out model prediction into CSV file (kaggle format)
 write.prediction <- function(model, testdata, fname="prediction.csv") {
     out <- predict(model, testdata)
+    o <- sapply(out, function(x) {if(is.na(x)) return(0) else as.integer(x)})
+    if (max(o) == 2) {
+        o <- sapply(out, function(x) {if(is.na(x)) return(0) else as.integer(x)-1})
+    }
     d <- data.frame(PassengerId=testdata$PassengerId)
-    o <- sapply(out, function(x) {if(is.na(x)) return(0) else as.integer(x)-1})
     d <- cbind(d, Survived=o)
     write.csv(d, fname)
     cmd <- sprintf("cat %s | sed -e \"s,\\\",,g\" | awk '{z=split($0,a,\",\"); print \"\"a[2]\",\"a[3]\"\"}' > %s.tmp; mv %s.tmp %s", fname, fname, fname, fname)
     system(cmd)
+}
+
+# helper function to divide original dataframe into two ones based on cut
+# usage. Should be run as following:
+# d1=cut1(train.data)
+# d2=cut2(train.data, d1)
+cut1 <- function(x) {
+    d <- subset(x, (x$Age<18 & x$Pclass<3) | (x$Sex=="female" & x$Pclass<3) |
+                    x$Embarked=="" | as.integer(x$Cabin)!=1)
+    return(d)
+}
+cut2 <- function(x, c1) {
+    ids <- c1$PassengerId
+    ndf <- data.frame()
+    for(i in 1:nrow(x)) {
+        if (!(x[i,]$PassengerId %in% ids)) {
+            ndf <- rbind(ndf, x[i,])
+        }
+    }
+    return(ndf)
+}
+
+# RandomForest function which takes: x - train dataset, y - test dataset
+run.rf <- function(tdf) {
+    cat("\n### Run RandomForest\n")
+    # run RandomForest, make sure that the variable used for classification is a
+    # factor. For prediction use the same dataset but exclude classification var.
+    rf.model <- randomForest(as.factor(Survived)~., data=tdf, importance=T, proximity=T)
+    print(rf.model)
+
+    # print important variables used in RF
+    imp <- importance(rf.model)
+    print(imp)
+    return(rf.model)
+}
+
+run.ksvm <- function(x) {
+    cat("\n### Run KSVM\n")
+    # PolyKernels
+    poly <- polydot(degree=1, scale=1, offset=0)
+    # RBF kernels
+    rbf <- rbfdot(sigma=1)
+
+    # kernel choice
+    k <- rbf
+
+    # type of classification
+    type <- sprintf("C-svc")
+    cross <- 10
+    cost <- 1
+
+    # run svm algorithm
+    ksvm.model <- ksvm(as.factor(Survived)~., data=x, type=type, cross=cross, kernel=k, C=cost, prob.model=T)
+    print(ksvm.model)
+    return(ksvm.model)
+}
+run.svm <- function(x) {
+    cat("\n### Run SVM\n")
+    # kernels
+#    k <- sprintf('polynomial')
+    k <- sprintf('radial')
+    degree <- 3
+    gamma <- 1
+    type <- sprintf('C-classification')
+    cross <- 10
+    cross <- 0
+
+    # run svm algorithm (e1071 library) for given vector of data and kernel
+    svm.model <- svm(as.factor(Survived)~., data=x,
+                 type=type, cross=cross, kernel=k, gamma=gamma, degree=degree)
+    print(svm.model)
+    return(svm.model)
+}
+cut.proc <- function(x, real.test=NULL, fname="rf") {
+    d1 <- cut1(x)
+    d2 <- cut2(x, d1)
+    par(mfrow=c(2,1))
+    hist(d1$Survived)
+    hist(d2$Survived)
+    d1 <- preprocess(d1)
+    d2 <- preprocess(d2)
+    d1 <- drop(d1, c("id", "PassengerId"))
+    d2 <- drop(d2, c("id", "PassengerId"))
+    test.d1 <- drop(d1, c("id", "PassengerId", "Survived"))
+    test.d2 <- drop(d2, c("id", "PassengerId", "Survived"))
+    f1 <- run.rf(d1)
+    f2 <- run.rf(d2)
+    pred1 <- predict(f1, test.d1)
+    pred2 <- predict(f2, test.d2)
+    cat("\n### Fit dataset1 ###\n")
+    conf.matrix(d1$Survived, pred1)
+#    imp1 <- importance(f1)
+#    print(imp1)
+    cat("\n### Fit dataset2 ###\n")
+    print(f2)
+    conf.matrix(d2$Survived, pred2)
+#    imp2 <- importance(f2)
+#    print(imp2)
+    if (!is.null(real.test)) {
+        test1 <- cut1(real.test)
+        test2 <- cut2(real.test, test1)
+        test1 <- preprocess(test1, survived=F)
+        test2 <- preprocess(test2, survived=F)
+        test1 <- drop(test1, c("id", "Survived"))
+        test2 <- drop(test2, c("id", "Survived"))
+        fname1 <- paste0(fname, "1.csv")
+        fname2 <- paste0(fname, "2.csv")
+#        min.cid <- min(test2$CabinId)
+#        max.cid <- max(test2$CabinId)
+#        if (min.cid==max.cid & min.cid == 1) { # it is constant
+#            test2$CabinId[1] = 2 # introduce fake value to allow SVM to run
+#        }
+        write.prediction(f1, test1, fname1)
+        write.prediction(f2, test2, fname2)
+        cmd <- sprintf("cat %s %s | sort -n -u > %s_double.csv", fname1, fname2, fname)
+        system(cmd)
+    }
 }
