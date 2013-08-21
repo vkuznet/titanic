4104b7e1e2c45b12bdf8aaf55bae586fb14bd6f7
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-21 10:16:39 -0400

    Code re-factoring, wrap SVM into function

--- src/R/svm.R
+++ src/R/svm.R
@@ -2,40 +2,37 @@
 # clean-up session parameters
 #rm(list=ls())
 
+do.svm <- function(tdf, testdata, fname="svm") {
+    # exclude id columne to work with ML
+    train.df <- drop(tdf, c("id", "PassengerId"))
+    # during training we use the same dataset, but exclude classification var
+    test.df <- drop(tdf, c("id", "PassengerId", "Survived"))
 
-# load libraries, helper functions, set seed.
-source("src/R/helper.R")
+    # this is an example on how to split data into train/test datasets
+    #index <- 1:nrow(df)
+    #testindex <- sample(index, trunc(length(index)/3))
+    #testset <- df[testindex,]
+    #trainset <- df[-testindex,]
 
-# load data
-my.path <- paste0(getwd(), "/")
-file.name <- "model.csv"
-df <- read.csv(paste0(my.path, file.name), header=T)
+    # kernels
+    k <- sprintf('polynomial')
+    degree <- 3
+    gamma <- 1
+    type <- sprintf('C-classification')
+    cross <- 10
 
-# exclude id columne to work with ML
-train.df <- drop(df, c("id", "PassengerId"))
-# during training we use the same dataset, but exclude classification var
-test.df <- drop(df, c("id", "PassengerId", "Survived"))
+    # run svm algorithm (e1071 library) for given vector of data and kernel
+    svm.model <- svm(Survived~., data=train.df,
+                 type=type, cross=cross, kernel=k, gamma=gamma, degree=degree)
+    print(svm.model)
 
-# this is an example on how to split data into train/test datasets
-#index <- 1:nrow(df)
-#testindex <- sample(index, trunc(length(index)/3))
-#testset <- df[testindex,]
-#trainset <- df[-testindex,]
+    # the last column of this dataset is what we'll predict, so we'll exclude it
+    svm.pred <- predict(svm.model, test.df)
 
-# kernels
-k <- sprintf('polynomial')
-degree <- 3
-gamma <- 1
-type <- sprintf('C-classification')
-cross <- 10
+    # write out prediction
+    pfile <- sprintf("%s_prediction.csv", fname)
+    write.prediction(svm.model, testdata, pfile)
 
-# run svm algorithm (e1071 library) for given vector of data and kernel
-svm.model <- svm(Survived~., data=train.df,
-             type=type, cross=cross, kernel=k, gamma=gamma, degree=degree)
-print(svm.model)
-
-# the last column of this dataset is what we'll predict, so we'll exclude it
-svm.pred <- predict(svm.model, test.df)
-
-# print confugtion matrix
-conf.matrix(train.df$Survived, svm.pred)
+    # print confugtion matrix
+    conf.matrix(train.df$Survived, svm.pred)
+}
