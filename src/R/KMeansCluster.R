fc754e1086904e26017a983fc782c637ebd945f1
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-21 10:15:12 -0400

    Code re-factoring

--- src/R/KMeansCluster.R
+++ src/R/KMeansCluster.R
@@ -1,19 +1,20 @@
 #!/usr/bin/env Rscript
 # clean-up session parameters
 #rm(list=ls())
+
 # load libraries, helper functions, set seed.
 source("src/R/helper.R")
 require(graphics)
 require(cluster)
 require(fpc)
 
-
+# helper function to create clusters
 RunClusters<-function(x,NumCluster=2){
   #Run cluster
   Cluster<-kmeans(x,NumCluster)
   #print Results
   print (Cluster)
-  #Plot Clusters 
+  #Plot Clusters
   # Make Plot Clusters
   fig.name <- paste0("plots/KMeansClusterOf",NumCluster,"nodesType1", ext)
   start.plot(fig.name)
@@ -22,7 +23,7 @@ RunClusters<-function(x,NumCluster=2){
   #plot Centers
   points(Cluster$centers,col=1:2,pch=8)
   dev.off()
-  #makePlot 2 
+  #makePlot 2
   fig.name <- paste0("plots/KMeansClusterOf",NumCluster,"nodesType2", ext)
   start.plot(fig.name)
   par(mfrow=c(1,1))
@@ -32,24 +33,19 @@ RunClusters<-function(x,NumCluster=2){
   fig.name <- paste0("plots/KMeansClusterOf",NumCluster,"nodesType3", ext)
   start.plot(fig.name)
   par(mfrow=c(1,1))
-  clusplot(train.df,Cluster$cluster,color=TRUE,shade=TRUE,labels=2,lines=0)
+  clusplot(x,Cluster$cluster,color=TRUE,shade=TRUE,labels=2,lines=0)
   dev.off()
   return(Cluster)
 }
 
-# load data
-my.path <- paste0(getwd(), "/")
-file.name <- "model.csv"
-df <- read.csv(paste0(my.path, file.name), header=T)
-#set the number of cluster to run 
-nc=2
+do.clustering <- function(tdf) {
+    #set the number of cluster to run
+    nc=2
 
-# exclude id columne to work with ML
-train.df <- drop(df, c("id", "PassengerId"))
-# during training we use the same dataset, but exclude classification var
-test.df <- drop(df, c("id", "PassengerId", "Survived"))
+    # exclude id columne to work with ML
+    train.df <- drop(tdf, c("id", "PassengerId", "Survived"))
 
-for(i in 2:10){
-  RunClusters(train.df,i)
+    for(i in 2:10){
+      RunClusters(train.df,i)
+    }
 }
-
