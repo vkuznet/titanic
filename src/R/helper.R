7e8ea817586d4f4e5a94b842d8b928df0daedb06
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-22 14:17:26 -0400

    Re-factor the code; took away plots/cabin function and put them into separate plot/data_helpers.R; added break.down helper function for splitting given attribute into binary form

--- src/R/helper.R
+++ src/R/helper.R
@@ -6,156 +6,29 @@ library(ggplot2)
 library(randomForest)
 library(e1071)
 library(kernlab)
+library(nnet)
+library(MASS)
 
 # set seed
 set.seed(12345)
 
-# use PDF for high-quality plots, while png for rmd
-#ext <- ".pdf"
-ext <- ".png"
-
-# helper function to initialize plotting function depending on file extention
-start.plot <- function(f) {
-    if (grepl("png$", f))
-        png(f, bg="transparent")
-    else if (grepl("pdf$", f))
-        pdf(f)
-    else if (grepl("jpg$", f))
-        jpeg(f)
-}
-
-# helper functino to reset plot layout
-graph.reset <- function() {
-    par(mfrow=c(1,1))
-    dev.off()
-}
+source("src/R/plot_helpers.R")
+source("src/R/data_helpers.R")
 
 # helper function to drop columns from given dataset
 drop <- function(df, drops) {
     return(df[,!names(df) %in% drops])
 }
 
-# helper function to assign Cabin category
-cabin.category <- function(x) {
-    llist <- LETTERS
-    cat <- 0
-    for(i in 1:length(llist)) {
-        pat <- sprintf("^%s" , llist[i])
-        if (grepl(pat, x)) {
-            return(i)
-        }
-    }
-    return(cat)
-}
-
-# helper function to re-assign cabin based on ticket info
-assign.cabin <- function(x) {
-    d <- x[with(x, order(-TicketId, -CabinCat)), ]
-    tid <- -1
-    for(i in 1:nrow(d)) {
-        if (tid == d[i,]$TicketId) { # tickets are the same
-            d[i,]$CabinCat <- d[i-1,]$CabinCat
-        } else {
-            tid <- d[i,]$TicketId
-        }
-    }
-    d <- d[with(d, order(PassengerId)),]
-    return (d)
-}
-
-# make plots for various attributes of given dataframe
-make.plots <- function(df.orig) {
-    print(sprintf("Make plots"))
-    # make new data frame with characters for mosaic plot
-    df.ch <- data.frame(Sex=df.orig$Sex)
-    df.ch$Class <- sapply(df.orig$Pclass,
-    function(x) {
-        if(x==1) return("1st")
-        else if(x==2) return("2nd")
-        else if(x==3) return("3rd")
-        else return("Crew")
-    })
-    #df.ch$Sex <- sapply(df.orig$Sex, function(x) {if(x=="male") return("Male") else return("Female")})
-    df.ch$Sec <- df.orig$Sex
-    df.ch$Survived <- sapply(df.orig$Survived, function(x) {if(x==1) return("Yes") else return("No")})
-    df.ch$Age <- sapply(df.orig$Age,
-    function(x) {
-        if(is.na(x)) return("NA")
-        else if(x>age.thr) return("Adult")
-        else return("Child")
-    })
-
-    # make mosaic plot of few attributes
-    fig.name <- paste0("mosaic", ext)
-    start.plot(fig.name)
-    mosaic(~Class+Sex+Age+Survived, data=df.ch, shade=TRUE, legend=TRUE)
-    dev.off()
-
-    # make plots of SibSp, Ticket, Parch vs Pclass
-    sdf <- subset(df.orig, df.orig$Survived==1)
-    nsdf <- subset(df.orig, df.orig$Survived==0)
-
-    fig.name <- paste0("class_plots1", ext)
-    start.plot(fig.name)
-    par(mfrow=c(2,2))
-    plot(sdf$SibSp, sdf$Pclass, xlab="SibSp", ylab="Class", main="Survived")
-    plot(nsdf$SibSp, nsdf$Pclass, xlab="SibSp", ylab="Class", main="Not-Survived")
-    plot(sdf$Parch, sdf$Pclass, xlab="Parch", ylab="Class", main="Survived")
-    plot(nsdf$Parch, nsdf$Pclass, xlab="Parch", ylab="Class", main="Not-Survived")
-    dev.off()
-    fig.name <- paste0("class_plots2", ext)
-    start.plot(fig.name)
-    par(mfrow=c(2,2))
-    plot(sdf$Age, sdf$Pclass, xlab="Age", ylab="Class", main="Survived")
-    plot(nsdf$Age, nsdf$Pclass, xlab="Age", ylab="Class", main="Not-Survived")
-    plot(sdf$Embarked, sdf$Pclass, xlab="Embarked", ylab="Class", main="Survived")
-    plot(nsdf$Embarked, nsdf$Pclass, xlab="Embarked", ylab="Class", main="Not-Survived")
-    dev.off()
-    fig.name <- paste0("class_plots3", ext)
-    start.plot(fig.name)
-    par(mfrow=c(1,2))
-    plot(sdf$Sex, sdf$Pclass, xlab="Sex", ylab="Class", main="Survived")
-    plot(nsdf$Sex, nsdf$Pclass, xlab="Sex", ylab="Class", main="Not-Survived")
-    dev.off()
-    fig.name <- paste0("class_plots4", ext)
-    start.plot(fig.name)
-    par(mfrow=c(1,2))
-    hist(df.orig$Survived, main="Survived rate", xlab="no cuts")
-    hist(subset(df.orig, (df.orig$Age<18 & df.orig$Pclass<3) | (df.orig$Sex=="female" & df.orig$Pclass<3) | df.orig$Embarked=="")$Survived, main="Survived rate", xlab="age<18&class<3|sex=F&class<3|not-embarked")
-    dev.off()
-
-    # survival plots by class
-    brks <- seq(0,80,5)
-    fig.name <- paste0("survival_age", ext)
-    start.plot(fig.name)
-    par(mfrow=c(1,1))
-    p0=hist(subset(train.data, train.data$Survived==0)$Age, breaks=brks)
-    p1=hist(subset(train.data, train.data$Survived==1)$Age, breaks=brks)
-    plot(p0, col=rgb(0,0,1,1/4), xlim=c(0,80), main="Survived", xlab="blue/red non/survived")
-    plot(p1, col=rgb(1,0,0,1/4), xlim=c(0,80), add=T, main="Survived", xlab="blue/red non/survived")
-    dev.off()
-
-}
-
-# make correlation plot
-make.cor.plot <- function (x) {
-    df <- drop(x, c("Survived"))
-    print(sprintf("Make correlation plot"))
-    # make correlation matrix
-    idx <- 2
-    cor.matrix <- cor(df[,idx:ncol(df)])
-    colnames(cor.matrix) <- names(df)[idx:ncol(df)]
-    rownames(cor.matrix) <- names(df)[idx:ncol(df)]
-    correlations <- as.numeric(cor.matrix)
-
-    # round to 2 digits correlation matrix and plot it
-    fig.name <- paste0("cor", ext)
-    start.plot(fig.name)
-    cor <- round(cor.matrix, digits=2)
-    corrplot(cor, method="shade", shade.col=NA, tl.col="black", tl.srt=45, addCoef.col="black", bg="transparent")
-    dev.off()
-#    cat("====== Correlations ======\n")
-#    print(cor)
+# helper function to get sample index for given dataframe
+# then someone can divided training/test set into the following:
+#    testset <- df[testindex,]
+#    trainset <- df[-testindex,]
+sample.index <- function(x) {
+    # this is an example on how to split data into train/test datasets
+    index <- 1:nrow(x)
+    testindex <- sample(index, trunc(length(index)/3))
+    return(testindex)
 }
 
 # helper function to calculate error of predicion
@@ -349,4 +222,26 @@ cut.proc <- function(x, real.test=NULL, fname="rf") {
         cmd <- sprintf("cat %s %s | sort -n -u > %s_double.csv", fname1, fname2, fname)
         system(cmd)
     }
+}
+
+# helper function to break down attribute into binary form for a given dataset
+break.down <- function(x, attr, drop=T) {
+    values <- unique(x[,c(attr)])
+    for(i in 1:nrow(x)) {
+        val <- x[i,c(attr)]
+        for(j in values) {
+            cname <- sprintf("%s.%i", attr, j)
+            if (val == j) {
+                x[i,c(cname)] <- 1
+            } else {
+                x[i,c(cname)] <- 0
+            }
+        }
+    }
+    if (drop==T) {
+        d <- drop(x, c(attr))
+    } else {
+        d <- x
+    }
+    return(d)
 }
