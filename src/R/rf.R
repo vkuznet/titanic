2dd367847e3548a6bcd23cc6f1e32578dfabfb37
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-23 17:10:21 -0400

    Add splitting

--- src/R/rf.R
+++ src/R/rf.R
@@ -2,11 +2,26 @@
 # clean-up session parameters
 #rm(list=ls())
 
-do.rf <- function(tdf, testdata, fname="rf", mtry=NULL) {
+do.rf <- function(tdf, testdata, fname="rf", mtry=NULL, split=FALSE, printModel=FALSE) {
     # exclude id columne to work with ML
     train.df <- drop(tdf, c("id", "PassengerId"))
     # during training we use the same dataset, but exclude classification var
     test.df <- drop(train.df, c("Survived"))
+    survived <- train.df$Survived
+
+    # use 70/30 splitting
+    if (split==FALSE) {
+        print(sprintf("Run RandomForest, use full training set"))
+    } else {
+        index <- 1:nrow(train.df)
+        testindex <- sample(index, trunc(length(index)/3))
+        testset <- train.df[testindex,]
+        trainset <- train.df[-testindex,]
+        train.df <- trainset
+        test.df <- drop(testset, c("Survived"))
+        survived <- testset$Survived
+        print(sprintf("Run RandomForest, train %d, test %d", nrow(trainset), nrow(testset)))
+    }
 
     # run RandomForest, make sure that the variable used for classification is a
     # factor. For prediction use the same dataset but exclude classification var.
@@ -16,30 +31,30 @@ do.rf <- function(tdf, testdata, fname="rf", mtry=NULL) {
         rf.model <- randomForest(Survived~., data=train.df, importance=T, proximity=F, mtry=mtry)
     }
     rf.pred <- predict(rf.model, test.df)
-    print(rf.model)
+    if(printModel==TRUE) print(rf.model)
 
     # write out prediction
     pfile <- sprintf("%s_prediction.csv", fname)
     write.prediction(rf.model, testdata, pfile)
 
     # print confugtion matrix
-    conf.matrix(train.df$Survived, rf.pred)
+    conf.matrix(survived, rf.pred)
 
     # make RF plots
-    fig.name <- paste0("rf1", ext)
-    start.plot(fig.name)
-    par(mfrow=c(1,1))
-    plot(rf.model)
-    dev.off()
-    fig.name <- paste0("rf2", ext)
-    start.plot(fig.name)
-    par(mfrow=c(1,2))
-    varImpPlot(rf.model)
-    dev.off()
+#    fig.name <- paste0("rf1", ext)
+#    start.plot(fig.name)
+#    par(mfrow=c(1,1))
+#    plot(rf.model)
+#    dev.off()
+#    fig.name <- paste0("rf2", ext)
+#    start.plot(fig.name)
+#    par(mfrow=c(1,2))
+#    varImpPlot(rf.model)
+#    dev.off()
 
     # print important variables used in RF
     imp <- importance(rf.model)
-    print(imp)
+    if(printModel==TRUE) print(imp)
 
     #fig.name <- paste0("partial_rf", ext)
     #start.plot(fig.name)
