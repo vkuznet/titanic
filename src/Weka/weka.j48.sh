b1fd117eed1d96c2fa48e24d5a47db50da800e1a
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-27 15:33:36 -0400

    Add different parametres

--- src/Weka/weka.j48.sh
+++ src/Weka/weka.j48.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # set path to Weka
-export CLASSPATH=/Applications/weka-3-6-9.app/Contents/Resources/Java/weka.jar
+#export CLASSPATH=/Applications/weka-3-6-9.app/Contents/Resources/Java/weka.jar
+export CLASSPATH=/Applications/weka-3-7-10-apple-jvm.app/Contents/Resources/Java/weka.jar
 data=./model.arff
 seed=1
 opts="-C 0.25 -M 2"
+opts="-C 0.1 -B -M 2 -A"
 
 if [ $# -eq 0 ]; then
 java weka.classifiers.trees.J48
