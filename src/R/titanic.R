bc8309e05881a90c2d1e27843603be1e8dfd033a
Valentin Kuznetsov <vkuznet@gmail.com>
2013-08-27 15:32:50 -0400

    More code re-organization

--- src/R/titanic.R
+++ src/R/titanic.R
@@ -3,11 +3,17 @@
 rm(list=ls())
 
 # set Age threshold to divide Adults and Children
-age.thr <- 10
-#age.thr <- 18
+#age.thr <- 8
+#age.thr <- 10
+age.thr <- 12
 
 # load helper functions, libraries, set seeds, etc.
 source("src/R/helper.R")
+source("src/R/aux.R")
+
+# read aux data
+print(sprintf("Read TITANICA data"))
+tit.data <- aux.data()
 
 # reset previous graphics settings and close all plot windows
 graph.reset()
@@ -17,9 +23,26 @@ my.path <- paste0(getwd(), "/data/")
 file.name <- "train.csv"
 train.data <- read.csv(paste0(my.path, file.name), header=T)
 orig.train.df <- train.data
+orig.train.df$sname <- sapply(orig.train.df$Name, assign.sname)
+orig.train.df$T <- sapply(orig.train.df$Ticket, assign.ticket)
 file.name <- "test.csv"
 test.data <- read.csv(paste0(my.path, file.name), header=T)
 orig.test.df <- test.data
+orig.test.df$sname <- sapply(orig.test.df$Name, assign.sname)
+orig.test.df$T <- sapply(orig.test.df$Ticket, assign.ticket)
+
+# adjust our train/test data wrt aux.data
+merged.train.df <- merge.with.aux(tit.data, orig.train.df)
+merged.test.df <- merge.with.aux(tit.data, orig.test.df)
+
+# re-assign train/test data to merged ones
+train.data <- merged.train.df
+test.data <- merged.test.df
+
+# get all names form both train and test sets to form unique set
+names.1 <- sapply(train.data$Name, function(x) {tolower(unlist(strsplit(as.character(x), ","))[1])})
+names.2 <- sapply(test.data$Name, function(x) {tolower(unlist(strsplit(as.character(x), ","))[1])})
+f.names <- unique(c(names.1, names.2))
 
 # get all tickets from both train and test sets to form unique set
 tickets.1 <- sapply(train.data$Ticket, function(x) {as.character(x)})
@@ -37,26 +60,32 @@ set.seed(12345)
 # adjust our input data
 adjust.data <- function(x, debug=F) {
     ndf <- data.frame()
-    adult.age <- mean(x$Age, na.rm=T)
-    kid.age <- age.thr-1
+#    adult.age <- mean(x$Age, na.rm=T)
+#    kid.age <- age.thr-1
+    adult.age <- runif(1, age.thr+1, max(x$Age, na.rm=T))
+    kid.age <- runif(1, 0, age.thr-1)
     for(i in 1:nrow(x)) {
         row <- x[i,]
         row$Parch <- adjust.Parch(row)
         row$SibSp <- adjust.SibSp(row)
+        row$SP <- row$SibSp+row$Parch
         row$Age <- row$Age #will be replaced in second loop
         row$Child <- 0 # placeholder
+        row$W <- 0
 #        row$Age <- assign.age(row, adult.age, kid.age)
 #        row$Child <- assign.child(row, age.thr) # run after Parch/SibSp/Age
-#        row$W <- assign.weigth(row, age.thr) # run after we assign age
         row$Family <- assign.family(row)
+        row$fname<- assign.fname(row)
         row$Title <- assign.title(row)
-        row$TicketId <- assign.tid(row, tickets)
+        row$tid <- assign.tid(row, tickets)
         row$Embarked <- assign.eid(row)
-        row$CabinCat <- cabin.category(row) # run after assigned TicketId
-        row$CabinId <- assign.cid(row, cabins)
+        row$ccat <- cabin.category(row) # run after assigned tid
+        row$cid <- assign.cid(row, cabins)
         row$Fare <- assign.fare(row)
         row$Gender <- assign.gender(row)
-        row$Fbin <- assign.bin(row, "Fare", c(0, 10, 30, 100))
+        row$fid <- assign.fid(row, f.names)
+#        row$Fbin <- assign.bin(row, "Fare", c(0, 10, 30, 100))
+        row$Fbin <- assign.bin(row, "Fare", seq(0,100,10))
         if (debug == T) {
             print(row)
         }
@@ -66,15 +95,17 @@ adjust.data <- function(x, debug=F) {
         row <- ndf[i,]
         row$Age <- assign.age(ndf, row, adult.age, kid.age)
         row$Child <- assign.child(row, age.thr) # run after Parch/SibSp/Age
+        row$W <- assign.weigth(row, age.thr) # run after we assign age
         ndf[i,] <- row
     }
-    ndf$Fare <- scale(ndf$Fare)
-    ndf$Age <- scale(ndf$Age)
+#    ndf$Fare <- scale(ndf$Fare)
+#    ndf$Age <- scale(ndf$Age)
+    ndf$fname <- as.integer(as.factor(ndf$fname))
     return(ndf)
 }
 
 # pre-process input data
-preprocess <- function(df.orig, attrs=NULL, survived=T) {
+preprocess <- function(df.orig, bin.attrs=NULL, survived=T) {
 
     # Take all numeric attributes from original df and put it into new dataframe
     df.new <- df.orig
@@ -85,8 +116,12 @@ preprocess <- function(df.orig, attrs=NULL, survived=T) {
     # adjust family attribute based on ticket ids in given dataset
     df.new <- adjust.family(df.new)
 
+    # drop requested attributes
+#    if(!is.null(drop.attrs))
+#        df.new <- drop(df.new, drop.attrs)
+
     # break down attributes into binary form
-    df.new <- to.binary(df.new, attrs)
+    df.new <- to.binary(df.new, bin.attrs)
 
     # classifier attribute
     if (survived==T) {
@@ -107,66 +142,66 @@ preprocess <- function(df.orig, attrs=NULL, survived=T) {
 
 print(sprintf("Fix Age attribute of train data"))
 print(sprintf("Prior correction: # of missing Age: %d", nrow(subset(train.data, is.na(train.data$Age)))))
+print(subset(train.data, is.na(train.data$Age))[,c("PassengerId", "Name")])
 train.data <- adjust.data(train.data)
 print(sprintf("After correction: # of missing Age: %d", nrow(subset(train.data, is.na(train.data$Age)))))
 # make some plots for our data
-#make.plots(train.data)
+make.plots(train.data)
 
 # Fix Age attribute of test data
 print(sprintf("Fix Age attribute on test data"))
 print(sprintf("Prior correction: # of missing Age: %d", nrow(subset(test.data, is.na(test.data$Age)))))
+print(subset(test.data, is.na(test.data$Age))[,c("PassengerId", "Name")])
 test.data <- adjust.data(test.data)
 print(sprintf("After correction: # of missing Age: %d", nrow(subset(test.data, is.na(test.data$Age)))))
 
 # prepare data for ML algorithms
-#attrs <- c("Title", "SibSp", "Parch", "Pclass", "Embarked", "Fbin")
-attrs <- NULL # list of attributes to convert to binary form
+#drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin","Age","Embarked", "ccat", "Fbin", "SibSp", "Parch")
+#drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "Fbin", "SibSp", "Parch", "Title", "tid")
+#drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "Fbin", "tid", "SibSp", "Parch", "Age", "Fare")
+#drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "SibSp", "Parch", "Fbin", "W", "sname")
+#drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "Fbin", "W", "sname", "Title", "SP")
+#drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "Fbin", "W", "sname", "SibSp", "Parch", "tid")
+drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "Fbin", "W", "sname", "tid", "SP", "fname", "Family")
 print(sprintf("Drop attributes"))
-print(attrs)
+print(drop.attrs)
+
+# binary attributes
+bin.attrs <- NULL # list of attributes to convert to binary form
+#bin.attrs <- c("Title", "SibSp", "Parch", "Pclass", "Embarked", "Fbin", "SP")
+#bin.attrs <- c("Title", "SP", "ccat", "Pclass", "Embarked")
+print(sprintf("Binary attributes"))
+print(bin.attrs)
+
 print(sprintf("Preprocess train data"))
-real.train.df <- preprocess(train.data, attrs)
+real.train.df <- preprocess(train.data, bin.attrs)
 train.df <- real.train.df # working copy
 # prepare test.data for ML, we do not write survived attribute
 print(sprintf("Preprocess test data"))
-real.test.df <- preprocess(test.data, attrs, survived=F)
+real.test.df <- preprocess(test.data, bin.attrs, survived=F)
 test.df <- real.test.df # working copy
 
 # drop Survived attribute from real test dataset
 real.test.df <- drop(test.df, c("Survived"))
 
-# drop some attribute before writing
-drop.attrs <- c("id", "Ticket", "Name", "Sex", "Cabin", "CabinCat", "Fbin")
-train.df <- drop(train.df, drop.attrs)
-test.df <- drop(test.df, drop.attrs)
+# ready to go, print train/test data to stdout
 print(sprintf("Train data"))
 print(head(train.df))
 print(sprintf("Test data"))
 print(head(test.df))
 
+
 # make correlation plots for train/test data
-#make.cor.plot(train.df, "train_cor")
-#make.cor.plot(test.df, "test_cor")
+#make.cor.plot(train.df, "train_cor", drop.attrs)
+#make.cor.plot(test.df, "test_cor", drop.attrs)
 
 # make two subset datasets
 print(sprintf("Make sdf/ndf"))
 sdf <- subset(real.train.df, real.train.df$Survived==1)
 ndf <- subset(real.train.df, real.train.df$Survived==0)
 
-# write data out for SVM
-print(sprintf("Write model.csv"))
-write.csv(train.df, file="model.csv")
-cmd="cat model.csv | sed 's/\"\"/\"id\"/g' > t.csv; mv -f t.csv model.csv"
-system(cmd)
-
-# write data in arff format for Weka
-print(sprintf("Write model.arff"))
-write.arff(train.df, file="model.arff")
-cmd="cat model.arff  | sed \"s/Survived numeric/Survived {0,1}/g\" > t.arff; mv -f t.arff model.arff"
-system(cmd)
-# write test data in arff format
-write.arff(test.df, file="test.arff")
-cmd="cat test.arff  | sed -e \"s/Survived string/Survived {0,1}/g\" -e \"s/'//g\" > t.arff; mv -f t.arff test.arff"
-system(cmd)
+# write data
+write.model(train.df, drop.attrs)
 
 # Run R ML algorithms
 source("src/R/ksvm.R")
@@ -175,13 +210,31 @@ source("src/R/nnet.R")
 source("src/R/KMeansCluster.R")
 source("src/R/caret.R")
 
+# make a sample index
+index <- 1:nrow(train.df)
+testindex <- sample(index, trunc(length(index)/3))
+formula <- NULL
+
 print(sprintf("##### Benchmark KSVM #####"))
-do.ksvm(train.df, test.df)
-do.ksvm(train.df, test.df, split=T)
+#formula <- as.formula("Survived~SP*Fare*tid+ccat*cid+Gender")
+#formula <- as.formula("Survived~tid+SibSp+Parch+Age+Fare")
+ksvm.fit <- do.ksvm(train.df, test.df, drop.attrs, formula=formula)
+ksvm.fit <- do.ksvm(train.df, test.df, drop.attrs, formula=formula, testindex=testindex)
 
 print(sprintf("##### Benchmark RandomForest #####"))
-do.rf(train.df, test.df)
-do.rf(train.df, test.df, split=T)
+#formula <- as.formula("Survived~Pclass+Age*Family+Title+Gender+SP*Fare+tid+ccat*cid+Embarked")
+#formula <- as.formula("Survived~Pclass+Embarked+Child+W+Family+Title+ccat+cid+Gender")
+rf.fit <- do.rf(train.df, test.df, drop.attrs, formula=formula)
+rf.fit <- do.rf(train.df, test.df, drop.attrs, formula=formula, testindex=testindex)
+
+#par(mfrow=c(2,1))
+#hist(sdf$W, breaks=seq(0,1,0.1))
+#hist(ndf$W, breaks=seq(0,1,0.1))
+
+#fits <- data.frame(pid=train.df[testindex,]$PassengerId,
+#                   Survived=train.df[testindex,]$Survived,
+#                   ksvm=ksvm.fit, rf=rf.fit)
+#merge.pred(fits)
 
 #print(sprintf("Run NNET"))
 #do.nnet(df, test.df)
@@ -195,18 +248,5 @@ do.rf(train.df, test.df, split=T)
 #    run.caret(train.df, m)
 #}
 
-# re-run ML with additional cluster info
-print(sprintf("### Run Clustering ###"))
-#clusters <- do.clustering(df, 6:10)
-#nclusters <- c(3,4,5,6,7,8,9,10)
-nclusters <- c(7)
-set.seed(1)
-for(nc in nclusters) {
-    fname <- sprintf("rf%i", nc)
-    cat("\n--------------------------\n")
-    print(sprintf("Add cluster %d", nc))
-    train.dd <- add.cluster(train.df, nc)
-    test.dd <- add.cluster(test.df, nc)
-    do.ksvm(train.dd, test.dd, fname, split=T)
-    do.rf(train.dd, test.dd, fname, split=T)
-}
+# Test clustering
+#test.clustering(train.df, test.df, c(7,10,12))
